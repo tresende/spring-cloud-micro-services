c3927a3250d005f2fb6c7597774c534a1168cfa4
Thiago Resende <thiago.gcresende@gmail.com>
2019-11-23 12:18:50 -0300

    Netflix Spring Feign

---
+++ loja/src/main/java/br/com/thiagoresende/loja/client/FornecedorClient.java
@@ -0,0 +1,14 @@
+package br.com.thiagoresende.loja.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.thiagoresende.loja.controller.dto.InfoFornecedorDTO;
+
+@FeignClient("fornecedor")
+public interface FornecedorClient {
+
+	@RequestMapping("/info/{estado}")
+	InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+}
--- loja/src/main/java/br/com/thiagoresende/loja/service/CompraService.java
+++ loja/src/main/java/br/com/thiagoresende/loja/service/CompraService.java
@@ -1,12 +1,9 @@
 package br.com.thiagoresende.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.thiagoresende.loja.client.FornecedorClient;
 import br.com.thiagoresende.loja.controller.dto.CompraDTO;
 import br.com.thiagoresende.loja.controller.dto.InfoFornecedorDTO;
 
@@ -14,23 +11,12 @@ import br.com.thiagoresende.loja.controller.dto.InfoFornecedorDTO;
 public class CompraService {
 
 	@Autowired
-	private RestTemplate client;
-
-	@Autowired
-	private DiscoveryClient eurekaClient;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO compra) {
 
-		String url = "http://fornecedor/info/" + compra.getEndereco().getEstado();
-		System.out.println(url);
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(url, HttpMethod.GET, null,
-				InfoFornecedorDTO.class);
-
-		eurekaClient.getInstances("fornecedor").stream().forEach(fornecedor -> {
-			System.out.println("localhost:" + fornecedor.getPort());
-		});
-
-		System.out.println(exchange.getBody().getEndereco());
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		System.out.println(info.getEndereco());
 	}
 
 }
