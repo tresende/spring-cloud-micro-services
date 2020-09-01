5864c34436dc260598bef212a9ea62b010354c7a
Thiago Resende <thiago.gcresende@gmail.com>
2020-08-31 21:03:28 -0300

    hystrix

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,26 +12,39 @@ import br.com.alura.microservice.loja.model.Compra;
 
 @Service
 public class CompraService {
-	
-	@Autowired
-	private FornecedorClient fornecedorClient;
 
-	public Compra realizaCompra(CompraDTO compra) {
-		
-		final String estado = compra.getEndereco().getEstado();
-		
-		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
-		
-		InfoPedidoDTO infoPedido = fornecedorClient.realizaPedido(compra.getItens());
-		
-		Compra compraSalva = new Compra();
-		compraSalva.setPedidoId(infoPedido.getId());
-		compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
-		compraSalva.setEnderecoDestino(info.getEndereco());
-		
-		System.out.println(info.getEndereco());
-		
-		return compraSalva;
-	}
-	
+    @Autowired
+    private FornecedorClient fornecedorClient;
+
+    @HystrixCommand(fallbackMethod = "realizaCompraFallback")
+    public Compra realizaCompra(CompraDTO compra) {
+
+        final String estado = compra.getEndereco().getEstado();
+
+        InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
+
+        InfoPedidoDTO infoPedido = fornecedorClient.realizaPedido(compra.getItens());
+
+        Compra compraSalva = new Compra();
+        compraSalva.setPedidoId(infoPedido.getId());
+        compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
+        compraSalva.setEnderecoDestino(info.getEndereco());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(info.getEndereco());
+
+        return compraSalva;
+    }
+
+    public Compra realizaCompraFallback(CompraDTO compra) {
+        Compra compraFallback = new Compra();
+        compraFallback.setEnderecoDestino(compra.getEndereco().toString());
+        return compraFallback;
+    }
+
 }
