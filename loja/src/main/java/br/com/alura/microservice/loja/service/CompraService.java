f8f269ce94d1c032ed76a457357c173d069bf7a2
Thiago Resende <thiago.gcresende@gmail.com>
2020-09-03 17:06:58 -0300

    configuracoes da loja

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -30,11 +30,13 @@ public class CompraService {
         compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
         compraSalva.setEnderecoDestino(info.getEndereco());
 
+        /*
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        */
 
         System.out.println(info.getEndereco());
 
